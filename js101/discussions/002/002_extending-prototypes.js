33776c6600bab2f0782f2f7c91af63baca106cb5
Eric McGary <emcgary@resource.com>
2013-01-11 15:27:35 -0500

    Finalized discussion 002

--- js101/discussions/002/002_extending-prototypes.js
+++ js101/discussions/002/002_extending-prototypes.js
@@ -1,80 +1,132 @@
+/*global console:false */
+
 "use strict";
 
-// The vehicle contructor. The constructor is a bit different than it was presented in [disucssion one](/). We'll be using a single argument of type `Object` which will give us the ability to easily add new options in future development.
-var Vehicle = function(options) {
-	if(options !== undefined) {
-		for(var prop in options) {
-			// @TODO: talk about why we're checking this how we are here
-			if(options.hasOwnProperty(prop)){
-				this[prop] = options[prop];
-			}
-		}
+// The vehicle contructor.
+
+var Vehicle = function(color) {
+	if(color !== undefined) {
+		this.color = color;
 	}
 };
 
-// The vehicle prototype. Here, unlike in disucssion one we'll be using the `Object.create` method which enables us to easily extend one object with another. Because this is our *base* object for all vehicles the `Vehicle` prototype will just simply be extending an empty [object literal](https://developer.mozilla.org/en-US/docs/JavaScript/Guide/Values,_variables,_and_literals#Object_literals).
-//
-// @TODO: mention that this is not supported for certain processes
+// The vehicle prototype, unlike in disucssion one uses the `Object.create` method which gives us to ability to easily extend one object with another.
+
+// While the `Object.create` saves us from having to write our own implementation for creating a new object as an extention of another certain browsers are not supported. You can see support metrics [here](https://developer.mozilla.org/en-US/docs/JavaScript/Reference/Global_Objects/Object/create).
+
 Vehicle.prototype = Object.create({},{
-	// @TODO: talk about why we're setting the variable like this here?
-	// @TODO. list where you can read more about the settings available here
-	// @TODO: overwrite type here as a getter
+	
+	// It's worth noting that prototype properties added via `Object.create` have to be written in a certain manner.
+	
 	type:{
-		value:"vehicle",
-		writable:true
+		
+		// 'get' is known as a property descriptor. There are four types of property descriptors. 'get', 'set', 'value', 'configuable', 'writable' and `enumerable`. I won't go into to much detail about what all of these property descriptors do, but if you're interested you can read more about them [here](https://developer.mozilla.org/en-US/docs/JavaScript/Reference/Global_Objects/Object/defineProperty).
+
+		// All you need to know for this *discussion* is that when i access the type of a Vehicle instance with `instance.type` it will return "vehicle".
+
+		get:function(){
+			return "vehicle";
+		}
+
 	},
 	color:{
 		value:"grey",
 		writable:true
 	},
-	startUp:{
+	travel:{
 		value:function(){
-			console.log("Starting the " + this.color + " " + this.type + " up...");
-		},
-		writable:true
-	},
-	drive:{
-		value:function(distance){
-			this.startUp();
-			console.log("Driving the " + this.color + " " + this.type + " for " + distance);
-		},
-		writable:true
-	},
-	shutDown:{
-		value:function(){
-			console.log("Shutting the " + this.color + " " + this.type + " down...");
+			console.log("I haven't been told how to!");
 		},
 		writable:true
 	}
 });
 
-// @TODO: descibe the car constructor
-var Car = function(options) {
-	// @TODO: talk about why we reference through `this` this way
-	this.type = "car";
-	// @TODO: describe why we're calling the `Vehicle` constructor like this
-	Vehicle.call(this,options);
-}
+// Here we're creating a `Car` constructor just like we did with the `Vehicle` constructor
 
-// @TODO: talk about why we're extending this way and why we're passing `Vehicle.prototype`
+var Car = function(color) {
+
+	// `Vehicle.call` might look a little foriegn to those not familiar with the various native methods available to JavaScript Functions.
+	
+	// The `call` method calls (executes) a method of another object in the context of a different object (the calling object).Arguments can be passed as they are.
+	
+	// So here we are simply calling the `Vehicle` Constructor and passing the `Car` instance we're creating as the new context `Vehicle` should use.
+	
+	// If you want to read more about the native `call` method you can do so [here](https://developer.mozilla.org/en-US/docs/JavaScript/Reference/Global_Objects/Function/call). Although this is the extent in which we'll be using it in this *discussion*.
+	
+	Vehicle.call(this,color);
+
+};
+
+// Now to **extending prototypes**! Here we're again using `Object.create` to create a new object that will act as the `Car`'s prototype. However here we're passing our previously defined `Vehicle.prototype` as its initial object to extend.
+
 Car.prototype = Object.create(Vehicle.prototype,{
-	// @TODO: overwrite type here as a getter
-	turn:{
-		value:function(direction){
-			console.log("The " + this.color + " " + this.type + "'s turn signal shows " + direction + "...");
+	
+	// Here we're overwriting the type property and its descriptors to reflect a `type` of "car"
+	type:{
+		get:function(){
+			return "car";
+		}
+	},
+	
+	// Override the `travel` method of the `Vehicle.prototype` so that the `Car` instances know how to travel
+
+	travel:{
+		value:function() {
+			console.log("The " + this.color + " " + this.type + " takes to the open highway.");
 		},
 		writable:true
 	}
 });
 
-// @TODO: metion about the instance
-var car = new Car({
-	color:"red"
+var Boat = function(name,color){
+	this.name = name;
+	Vehicle.call(this,color);
+};
+
+Boat.prototype = Object.create(Vehicle.prototype,{
+	
+	// Here we're overwriting the type property and its descriptors to reflect a `type` of "boat"
+	type:{
+		get:function(){
+			return "boat";
+		}
+	},
+
+	// People like to name their boats !!!!
+	
+	name:{
+		value:"boat",
+		writable:true
+	},
+	travel:{
+		value:function() {
+			console.log("'" + this.name + "', which is a " + this.color + " " + this.type + ", sails the ocean blue.");
+		},
+		writable:true
+	}
 });
 
-// @TODO: explain why this is important
-car.drive("100 miles");
-car.turn("left");
-car.shutDown();
 
-// @TODO: possibly create like a boat vehicle... think about what is different for the two types car vs boat
+// Here we'll create some new instances of both our `Car` and `Boat` prototypes. Notice that when we wrote Constructors for each that we never actually told them how to handle the color argument, but since we ran the `Vehicle`'s Constructor method from within color is still applied.
+
+var car = new Car("red");
+var boat = new Boat("The Cod Father","white");
+
+// Because we've overridden the `travel` method for both of the vehicle types they will now produce differnt outcomes.
+
+// should log `The red car takes to the open highway.`
+
+car.travel();
+
+// should log `The Cod Father, which is a white boat, sails the ocean blue.`
+
+boat.travel();
+
+// Note that you can still create an instance of `Vehicle`, but because we haven't written any specific functionality on how it should `travel` it won't actually know how to.
+
+var vehicle = new Vehicle();
+
+// should log `I haven't been told how to!`
+
+vehicle.travel();
+
--- js101/discussions/002/README.md
+++ js101/discussions/002/README.md
@@ -2,9 +2,24 @@
 
 ### Overview
 
+In this disscussion we'll look at how you can use classical inheritance (which was discussed in [discussion one](https://github.com/mcgaryes/crumblies/tree/master/js101/discussions/001), `Object.create` and the `Function.call` method to create extendable JavaScript Objects.
+
+This discussion does not discuss the concept of JavaScript "mixins", the practice of inheriting from multiple object prototypes. That will be covered in the next discussion! So stay tuned! 
+
+### Discussion Questions
+
+* How would you possibly go about using this functionality in non-supported browsers?
+* How else could you possibly extend one prototype with another?
+* Have you ever used this type of functionality in any of your projects?
+
+### Further Reading
+
+* [Object.create](https://developer.mozilla.org/en-US/docs/JavaScript/Reference/Global_Objects/Object/create)
+* [Function.call](https://developer.mozilla.org/en-US/docs/JavaScript/Reference/Global_Objects/Function/call)
+* [Object.defineProperty](https://developer.mozilla.org/en-US/docs/JavaScript/Reference/Global_Objects/Object/defineProperty)
 
 ### Resources
 
-* [Annotated Source Code](http://emcgary.r1l4b.com/discussions/002_extending_prototypes.html)
+* [Annotated Source Code](http://emcgary.r1l4b.com/discussions/002_extending-prototypes.html)
 * [GitHub](https://github.com/mcgaryes/crumblies/blob/master/js101/discussions/002/)
-* [JSBin](http://jsbin.com/)
+* [JSBin](http://jsbin.com/urudug/1/edit)
